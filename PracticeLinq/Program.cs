b2bfd0d4a079b85bf26ad7752806a62aaa39d128
Casey Hudler <chudler624@gmail.com>
2022-08-26 17:00:56 -0400

    initial commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,13 @@
+﻿var videoGameNames= new List<string>()
+{ "Madden 22",
+  "Fifa World Cup", 
+  "Call of Duty",
+  "Roblox", 
+  "Clash of Clans", 
+  "CSR Racing", 
+  "Archero" };
+
+videoGameNames.OrderBy(x => x).ToList();
+
+foreach (var item in videoGameNames)
+    Console.WriteLine();
